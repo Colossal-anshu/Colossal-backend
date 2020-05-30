66c4a979f3a76a8901b5f335c90279df5bf211b9
Anshu <raianshu1998@gmail.com>
2020-05-30 19:40:46 +0530

    cors update

--- index.js
+++ index.js
@@ -1,99 +1,114 @@
-const express=require('express');
+const express = require("express");
 
-
-const app=express();
-require('./config/viewhelpers')(app);
-const port=7000;
+const app = express();
+require("./config/viewhelpers")(app);
+const port = 7000;
 
 // using express layouts for creating views layout
-const expresslayout=require('express-ejs-layouts');
-const db=require('./config/mongoose');
-const cokkiesparser=require('cookie-parser');
+const expresslayout = require("express-ejs-layouts");
+const db = require("./config/mongoose");
+const cokkiesparser = require("cookie-parser");
 
-const passportlocal=require('./config/passport-local');
-const passportjwt=require('./config/passport-jwt');
-const passportgoogle=require('./config/passport_googleoauth2');
-const MongoStore=require('connect-mongo')(session);
-const sassMiddleware=require('node-sass-middleware');
-const flash=require('connect-flash');
-const customMware=require('./config/middleware');
+const passportlocal = require("./config/passport-local");
+const passportjwt = require("./config/passport-jwt");
+const passportgoogle = require("./config/passport_googleoauth2");
+const MongoStore = require("connect-mongo")(session);
+const sassMiddleware = require("node-sass-middleware");
+const flash = require("connect-flash");
+const customMware = require("./config/middleware");
 
-// creating our chat server 
+// creating our chat server
 
-const chatSever=require('http').Server(app);
-const chatSocket=require('./config/chatsockcet').chatSocket(chatSever);
-const env=require('./config/enviorment');
-const path=require('path');
-const logger = require('morgan');
+const chatSever = require("http").Server(app);
+const chatSocket = require("./config/chatsockcet").chatSocket(chatSever);
+const env = require("./config/enviorment");
+const path = require("path");
+const logger = require("morgan");
 
 chatSever.listen(5000);
-console.log('chatserver is listning at port 5000');
+console.log("chatserver is listning at port 5000");
 
-// middlewares 
-
+// middlewares
 
 // if (env.name == 'development')
 // {
-app.use(sassMiddleware({
-    src: './assets/scss',
-    dest: './assets/css',
+const whitelist = [
+  "http://localhost:8080",
+  "http://localhost:3000",
+  "http://thecolossal.xyz",
+  "https://thecolossal.xyz",
+  "http://localhost",
+];
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) != -1) callback(null, true);
+    else callback(new Error("Not Allowed by CORS"));
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(
+  sassMiddleware({
+    src: "./assets/scss",
+    dest: "./assets/css",
     debug: true,
-    outputStyle: 'compressed',
-    prefix: '/css'
-}));
-
-
+    outputStyle: "compressed",
+    prefix: "/css",
+  })
+);
 
 // makeing uplod path avaialble for the browser
-
 
 app.use(expresslayout);
 app.use(express.urlencoded());
 app.use(cokkiesparser());
-app.use('/uploads',express.static(__dirname +'/uploads'));
+app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(logger(env.morgan.mode, env.morgan.options));
 
-app.use(express.static('./assets'));
+app.use(express.static("./assets"));
 
-
-app.use('/uploads',express.static(__dirname +'/uploads'));
+app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(logger(env.morgan.mode, env.morgan.options));
 
 // extract style and script from subpages to layouts
-app.set('layout extractStyles',true);
-app.set('layout extractScripts',true);
-
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 // using express router
 //app.use('/',require('./routes'));
 
 // seting up our view engine
 
-app.set('view engine','ejs');
-app.set('views','./views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-//  using session as middleware here 
+//  using session as middleware here
 
-app.use(session({
-     name: 'Asocial',
-     // TODO change the secret before deployment in production mode
-     secret: env. session_cookie_key,
-     saveUninitialized: false,
-     resave: false,
-     cookie: {
-         maxAge: (1000 * 60 * 100)
-     },
-     store: new MongoStore(
-         {
-             mongooseConnection: db,
-             autoRemove: 'disabled'
-         
-         },
-         function(err){
-             console.log(err ||  'connect-mongodb setup ok');
-         }
-     )
- }));
+app.use(
+  session({
+    name: "Asocial",
+    // TODO change the secret before deployment in production mode
+    secret: env.session_cookie_key,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+    store: new MongoStore(
+      {
+        mongooseConnection: db,
+        autoRemove: "disabled",
+      },
+      function (err) {
+        console.log(err || "connect-mongodb setup ok");
+      }
+    ),
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -102,13 +117,16 @@ app.use(flash());
 app.use(customMware.setflash);
 
 // using express router
-app.use('/',require('./routes'));
+app.use("/", require("./routes"));
 
 // firing server
 
-app.listen(port,function(err)
-{
-     if(err)  {console.log(`Error in running server:${port}`);return;}
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running server:${port}`);
+    return;
+  }
 
-     console.log(`Surver is up and Running at POrt :${port}`); return;
+  console.log(`Surver is up and Running at POrt :${port}`);
+  return;
 });
